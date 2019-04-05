8350acac71723320371183345a18bf13ef221742
vlly <vlly@gmx.de>
2019-04-05 15:25:34 +0200

    first commit

---
+++ thatupload/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thatupload
+//
+//  Created by Vlad Lytvynenko on 05.04.19.
+//  Copyright © 2019 Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
